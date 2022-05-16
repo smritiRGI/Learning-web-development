10be5fb5f688a9b8c2266bb81f75fafe958bebbb
Smriti Rastogi <smritirastogi33@gmail.com>
2022-05-16 23:30:43 +0530

    facebook login & signup page

--- facebook-clone/src/Login.js
+++ facebook-clone/src/Login.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import React , {useState, useHistory} from 'react';
+import {Form , Button , Input} from 'reactstrap';
+import {Link} from 'react-router-dom';
+import './Login.css';
+import Signup from './Signup';
 
 function Login() {
+  
+  const [emailValue , setEmail] = useState();
+  const [passwordValue , setPassword] = useState();
+  const [show, setShow] = useState(false);
+
+  const handleClose = ()=>setShow(false);
+
   return (
-    <div>Login</div>
+    <div className="login">
+        <div id="login-brand">
+            <img id="logo" src="https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg" width="300vw" alt="facebook logo"/>
+            <h2 id="logo-caption">Facebook helps you connect and share <br></br>with the people in your life.</h2>
+        </div>
+        <div id="login-form">
+            <Form>
+                <Input type="email" value={emailValue} id="email" 
+                placeholder="Email address or Phone Number" 
+                onChange={(e) => setEmail(e.target.value)}/>
+                <Input type="password" id="password" value={passwordValue} placeholder="Password" 
+                onChange={(e) => setPassword(e.target.value)} />
+                <Button variant="primary" id="login-button" type="submit" 
+                    disabled= {!emailValue || !passwordValue}>Log In</Button>
+                <br/>
+                <a id="forgot-password" href="/">Forgotten Password?</a>
+                <hr/>
+          </Form>
+          <Button className='signup-button' 
+          onClick={() => setShow(true)}>Create New Account</Button>
+          {show ? <Signup show={show}  handleClose={handleClose} /> : <div></div>}
+        </div>
+    </div>
   )
 }
 
---
+++ facebook-clone/src/Signup.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import {Modal , ModalBody,ModalFooter,ModalHeader,Button , Form,Input} from 'reactstrap';
+import './Signup.css';
+
+function Signup({show , handleClose}) {
+  const [firstName,setFirstName] = useState();
+  const [lastName,setLastName] = useState();
+  const [emailValue,setEmail] = useState();
+  const [passwordValue,setPassword] = useState();
+
+  return (
+    <>
+    <Modal isOpen={show} toggle={handleClose}>
+          <ModalHeader toggle={handleClose}><h2>Sign Up</h2><h6>It's quick and easy!</h6></ModalHeader>
+          <ModalBody>
+            <Form>
+              <span className='name'>
+                <Input type="text" value={firstName} id="fname" 
+                placeholder="First name" 
+                onChange={(e) => setFirstName(e.target.value)}/>
+                <Input type="text" id="lname" value={lastName} placeholder="Surname" 
+                onChange={(e) => setLastName(e.target.value)} />
+              </span>
+                <Input type="email" value={emailValue} id="email" 
+                placeholder="Email address or Phone Number" 
+                onChange={(e) => setEmail(e.target.value)}/>
+                <Input type="password" id="password" value={passwordValue} placeholder="Password" 
+                onChange={(e) => setPassword(e.target.value)} />
+                <h6 id="declaration">By clicking Sign Up, you agree to our Terms, Data Policy and Cookie Policy.
+                 You may receive SMS notifications from us and can opt out at any time.</h6>
+                <Button variant="primary" id="signup-button" type="submit" >Sign Up</Button>
+            </Form>
+          </ModalBody>
+    </Modal>
+    </>
+  )
+}
+
+export default Signup;
